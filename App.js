7b7392c4651611982fabe90fdc06d179bf97939e
Rafayisworking <160171665+Rafayisworking@users.noreply.github.com>
2024-02-19 15:54:57 +0000

    New Updates

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import AddExcercise from "./components/AddExcercise";
-import { Box, GluestackUIProvider } from "@gluestack-ui/themed";
-import { config } from "@gluestack-ui/config";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <GluestackUIProvider config={config}>
-        <Box width="100%" justifyContent="center" alignItems="center">
-          {/* <Text>Open up App.js to start working on your app!</Text> */}
-          <AddExcercise />
-        </Box>
-      </GluestackUIProvider>
+      <Text>Open up App.js to start working ojkhiun your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
